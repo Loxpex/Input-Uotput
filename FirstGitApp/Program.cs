9f15f91ca9c295733d7cde9ec3ccdba2629bf644
Loxpex <90549537+Loxpex@users.noreply.github.com>
2021-09-12 16:06:35 +0500

    Update Program.cs

--- FirstGitApp/Program.cs
+++ FirstGitApp/Program.cs
@@ -12,6 +12,8 @@ namespace ConsoleAppForGit
             int sum = 0;
             int dif = 0;
             double mod = 0;
+            double comp = 0;
+            double div = 0;
             try
             {
                 Console.Clear();
@@ -33,6 +35,10 @@ namespace ConsoleAppForGit
             Console.WriteLine("Разность двух чисел: {0}", dif);
             mod = ((double)Math.Abs(a) + (double)Math.Abs(b)) / 2;
             Console.WriteLine("Среднее арифметическое модулей: {0}", mod);
+            comp = a * b;
+            Console.WriteLine("Произведение двух чисел: {0}", comp);
+            div = Convert.ToDouble(a) / Convert.ToDouble(b);
+            Console.WriteLine("Частное двух чисел: {0}", div);
         }
     }
 }
