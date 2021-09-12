84d4343b8ee019de2929a49a0d9765401a272475
Loxpex <90549537+Loxpex@users.noreply.github.com>
2021-09-12 15:27:35 +0500

    Program was created

---
+++ ConsoleAppForGit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleAppForGit
+{
+    class Program
+    {
+        static void Main()
+        {
+            Back:
+            int a = 0;
+            int b = 0;
+            int sum = 0;
+            int dif = 0;
+            double mod = 0;
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("Введите первое число");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число");
+                b = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.Clear();
+                Console.WriteLine("Вы ввели не число, нажмите любую клваишу и попробуйте снова");
+                Console.ReadKey();
+                goto Back;
+            }
+            sum = a + b;
+            Console.WriteLine("Сумма двух чисел: {0}", sum);
+            dif = a - b;
+            Console.WriteLine("Разность двух чисел: {0}", dif);
+            mod = ((double)Math.Abs(a) + (double)Math.Abs(b)) / 2;
+            Console.WriteLine("Среднее арифметическое модулей: {0}", mod);
+
+        }
+    }
+}
